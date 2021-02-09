e59fbe2e470e277082def400a1cf959e2522ab9f
Kinga <niu.ula.a@poczta.fm>
2021-02-09 15:26:18 +0100

    Random Hole Position

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(-6.8f, 6.8f), transform.position.y, transform.position.z);
+    }
+}
