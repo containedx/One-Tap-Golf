9e22a3012aa5178f47ffbbbadd9f0823d932d437
Kinga <niu.ula.a@poczta.fm>
2021-02-09 18:02:27 +0100

    Throw Ball And Increase Force

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject PointPrefab;
+
+    public float force;
+
+    Vector2 direction;
+
+    
+    void Update()
+    {
+        if(Input.GetButton("Fire1")) //if LMB clicked
+        {
+            PaintParabola();
+            IncreaseForce();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ThrowBall();
+            Debug.Log("throw");
+        }
+    }
+
+    void ThrowBall()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)transform.position;
+
+        rb.AddForce(direction * force);
+    }
+
+    void IncreaseForce()
+    {
+        force += 1;
+    }
+
+    void PaintParabola()
+    {
+
+    }
+
+    Vector2 GetPointPosition(float x)
+    {
+        return Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public GameObject point;
+    void Update()
+    {
+        // Dont Show Points If LMB not clicked
+        if(Input.GetButtonUp("Fire1"))
+        {
+            Destroy(point);
+        }
+    }
+}
