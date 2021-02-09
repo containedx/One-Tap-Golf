5cdd873907502ed5cee4d4fb7da2087497706885
Kinga <niu.ula.a@poczta.fm>
2021-02-09 15:30:22 +0100

    Points Caption

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -6,6 +6,7 @@ public class Hole : MonoBehaviour
 {
     void Start()
     {
-        transform.position = new Vector3(Random.Range(-6.8f, 6.8f), transform.position.y, transform.position.z);
+        //Random Hole Position in Right Part Of Screen
+        transform.position = new Vector3(Random.Range(0f, 6.8f), transform.position.y, transform.position.z);
     }
 }
